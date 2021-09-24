daccbc6a708352198f40e99f6e1ade277aa65a23
charlie <youmax210139@gmail.com>
2021-09-24 11:51:03 +0700

    fix: remove auth method from permission middleware

--- routes/admin.php
+++ routes/admin.php
@@ -12,72 +12,77 @@ $api->group([
         'middleware' => [
             'api.auth:admin',
             'whitelist:admin',
-            'role_or_permission:Super Admin',
         ],
     ], function ($api) {
         $api->post('/auth/logout', ['as' => 'auth.logout', 'uses' => 'AuthController@logout']);
         $api->post('/auth/refresh', ['as' => 'auth.refresh', 'uses' => 'AuthController@refresh']);
         $api->post('/auth/me', ['as' => 'auth.me', 'uses' => 'AuthController@me']);
 
-        $api->resource('permissions', 'PermissionController', ['except' => ['show']]);
+        $api->group([
+            'middleware' => [
+                'role_or_permission:Super Admin,admin',
+            ],
+        ], function ($api) {
+            $api->resource('permissions', 'PermissionController', ['except' => ['show']]);
 
-        $api->resource('roles', 'RoleController', ['except' => ['show']]);
+            $api->resource('roles', 'RoleController', ['except' => ['show']]);
 
-        $api->resource('banks', 'BankController', ['except' => ['show']]);
+            $api->resource('banks', 'BankController', ['except' => ['show']]);
 
-        $api->resource('admins', 'AdminController', ['except' => ['show']]);
+            $api->resource('admins', 'AdminController', ['except' => ['show']]);
 
-        $api->resource('settings', 'SettingController', ['only' => ['index', 'update']]);
+            $api->resource('settings', 'SettingController', ['only' => ['index', 'update']]);
 
-        $api->resource('merchant_white_lists', 'MerchantWhiteListController', ['except' => ['show']]);
+            $api->resource('merchant_white_lists', 'MerchantWhiteListController', ['except' => ['show']]);
 
-        $api->resource('merchants', 'MerchantController', ['except' => ['show']]);
-        $api->put("/merchants/renew/{merchant}", [
-            'as' => 'merchants.renew', 'uses' => 'MerchantController@renew'
-        ]);
-        $api->put("/merchants/whitelist/{merchant}", [
-            'as' => 'merchants.whitelist', 'uses' => 'MerchantController@whitelist'
-        ]);
-        $api->put("/merchants/fee/{merchant}", [
-            'as' => 'merchants.fee', 'uses' => 'MerchantController@fee'
-        ]);
-        $api->put("/merchants/reset_password/{merchant}", [
-            'as' => 'merchants.reset_password', 'uses' => 'MerchantController@reset'
-        ]);
+            $api->resource('merchants', 'MerchantController', ['except' => ['show']]);
+            $api->put("/merchants/renew/{merchant}", [
+                'as' => 'merchants.renew', 'uses' => 'MerchantController@renew'
+            ]);
+            $api->put("/merchants/whitelist/{merchant}", [
+                'as' => 'merchants.whitelist', 'uses' => 'MerchantController@whitelist'
+            ]);
+            $api->put("/merchants/fee/{merchant}", [
+                'as' => 'merchants.fee', 'uses' => 'MerchantController@fee'
+            ]);
+            $api->put("/merchants/reset_password/{merchant}", [
+                'as' => 'merchants.reset_password', 'uses' => 'MerchantController@reset'
+            ]);
 
-        $api->resource('merchant_deposits', 'MerchantDepositController', ['only' => ['index', 'update']]);
-        $api->put("/merchant_deposits/resend/{merchant_deposit}", [
-            'as' => 'merchant_deposits.resend', 'uses' => 'MerchantDepositController@resend'
-        ]);
+            $api->resource('merchant_deposits', 'MerchantDepositController', ['only' => ['index', 'update']]);
+            $api->put("/merchant_deposits/resend/{merchant_deposit}", [
+                'as' => 'merchant_deposits.resend', 'uses' => 'MerchantDepositController@resend'
+            ]);
 
-        $api->resource('merchant_withdrawals', 'MerchantWithdrawalController', ['only' => ['index']]);
+            $api->resource('merchant_withdrawals', 'MerchantWithdrawalController', ['only' => ['index']]);
 
-        $api->resource('payment_channels', 'PaymentChannelController', ['except' => ['show']]);
+            $api->resource('payment_channels', 'PaymentChannelController', ['except' => ['show']]);
 
-        $api->resource('resellers', 'ResellerController', ['except' => ['show']]);
-        $api->put("/resellers/deposit/{reseller}", [
-            'as' => 'resellers.deposit', 'uses' => 'ResellerController@deposit'
-        ]);
-        $api->put("/resellers/withdrawal/{reseller}", [
-            'as' => 'resellers.withdrawal', 'uses' => 'ResellerController@withdrawal'
-        ]);
-        $api->put("/resellers/reset_password/{reseller}", [
-            'as' => 'resellers.reset_password', 'uses' => 'ResellerController@reset'
-        ]);
+            $api->resource('resellers', 'ResellerController', ['except' => ['show']]);
+            $api->put("/resellers/deposit/{reseller}", [
+                'as' => 'resellers.deposit', 'uses' => 'ResellerController@deposit'
+            ]);
+            $api->put("/resellers/withdrawal/{reseller}", [
+                'as' => 'resellers.withdrawal', 'uses' => 'ResellerController@withdrawal'
+            ]);
+            $api->put("/resellers/reset_password/{reseller}", [
+                'as' => 'resellers.reset_password', 'uses' => 'ResellerController@reset'
+            ]);
 
-        $api->resource('reseller_bank_cards', 'ResellerBankCardController', ['only' => ['index', 'update', 'destroy']]);
+            $api->resource('reseller_bank_cards', 'ResellerBankCardController', ['only' => ['index', 'update', 'destroy']]);
 
-        $api->resource('reseller_deposits', 'ResellerDepositController', ['only' => ['index']]);
+            $api->resource('reseller_deposits', 'ResellerDepositController', ['only' => ['index', 'update']]);
 
-        $api->resource('reseller_withdrawals', 'ResellerWithdrawalController', ['only' => ['index', 'update']]);
+            $api->resource('reseller_withdrawals', 'ResellerWithdrawalController', ['only' => ['index', 'update']]);
 
-        $api->get("/report/resellers", [
-            'uses' => "ReportController@reseller",
-            'as' => "report_resellers.index"
-        ]);
-        $api->get("/report/merchants", [
-            'uses' => "ReportController@merchant",
-            'as' => "report_merchants.index"
-        ]);
+            $api->get("/report/resellers", [
+                'uses' => "ReportController@reseller",
+                'as' => "report_resellers.index"
+            ]);
+            $api->get("/report/merchants", [
+                'uses' => "ReportController@merchant",
+                'as' => "report_merchants.index"
+            ]);
+        });
     });
 });
