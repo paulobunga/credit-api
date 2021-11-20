89d3ed7308d304963ee467c8d2e8aeecb0262bb2
charlie <youmax210139@gmail.com>
2021-11-20 12:58:58 +0700

    feat: add reseller sms index action

---
+++ app/Http/Controllers/Admin/ResellerSmsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Dingo\Api\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+use App\Models\ResellerSms;
+
+class ResellerSmsController extends Controller
+{
+    protected $model = ResellerSms::class;
+
+    protected $transformer = \App\Transformers\Admin\ResellerSmsTransformer::class;
+
+    /**
+     * Get SMS list
+     *
+     * @param \Dingo\Api\Http\Request $request
+     * @method GET
+     * @return json
+     */
+    public function index(Request $request)
+    {
+        $m = QueryBuilder::for($this->model)
+            ->with([
+                'reseller'
+            ])
+            ->join('resellers', 'resellers.id', '=', 'reseller_sms.reseller_id')
+            ->select(
+                'reseller_sms.*',
+                'resellers.name'
+            )
+            ->allowedFilters([
+                AllowedFilter::partial('name', 'resellers.name'),
+                AllowedFilter::exact('status'),
+            ])
+            ->allowedSorts([
+                'id',
+                'status'
+            ]);
+
+        return $this->paginate($m, $this->transformer);
+    }
+}
