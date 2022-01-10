3ff9b553839f4c1581228890643cf2282aa980f1
charlie <youmax210139@gmail.com>
2022-01-10 15:33:42 +0700

    fix: NAGAD format

--- app/Payments/NAGAD/BDT.php
+++ app/Payments/NAGAD/BDT.php
@@ -10,7 +10,7 @@ class BDT
         'wallet_number'
     ];
 
-    public $sms_rule = '/Tk ([\d,]+\.\d{2})* Customer: (\d+)* TxnID: (\w+)* Comm: Tk ([\d,]+\.\d{2})* Balance: Tk ([\d,]+\.\d{2})*/';
+    public $sms_rule = '/Tk ([\d,]+\.\d{2})*\nCustomer: (\d+)*\nTxnID: (\w+)*\nComm: Tk ([\d,]+\.\d{2})*\nBalance: Tk ([\d,]+\.\d{2})*/';
 
     public $sms_map = [
         'amount' => 1,
