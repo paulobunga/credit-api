5f028070a7f21ec128e2c98ebb1bfb9aa6eafe7f
charlie <youmax210139@gmail.com>
2022-01-17 12:19:11 +0700

    feat: remove demo routes in production

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,15 @@ $api->group([
         'domain:' . env('PUBLIC_DOMAIN'),
     ],
 ], function ($api) {
-    $api->get("/demos/payin", ['as' => 'demos.payin.create', 'uses' => 'DemoController@payin']);
-    $api->post("/demos/payin", ['as' => 'demos.payin.store', 'uses' => 'DemoController@payin']);
-
-    $api->get("/demos/payout", ['as' => 'demos.payout.create', 'uses' => 'DemoController@payout']);
-    $api->post("/demos/payout", ['as' => 'demos.payout.store', 'uses' => 'DemoController@payout']);
-
-    $api->post("/demos/callback", ['as' => 'demos.callback', 'uses' => 'DemoController@callback']);
+    if (env('APP_ENV') !== 'production') {
+        $api->get("/demos/payin", ['as' => 'demos.payin.create', 'uses' => 'DemoController@payin']);
+        $api->post("/demos/payin", ['as' => 'demos.payin.store', 'uses' => 'DemoController@payin']);
+        $api->get("/demos/payout", ['as' => 'demos.payout.create', 'uses' => 'DemoController@payout']);
+        $api->post("/demos/payout", ['as' => 'demos.payout.store', 'uses' => 'DemoController@payout']);
+        $api->post("/demos/callback", ['as' => 'demos.callback', 'uses' => 'DemoController@callback']);
+    }
 
     $api->get("/pay/deposits", ['as' => 'deposits.pay', 'uses' => 'DepositController@pay']);
-
     $api->get("/pay/withdrawals", ['as' => 'withdrawals.pay', 'uses' => 'WithdrawalController@pay']);
 
     $api->group([
@@ -24,6 +23,7 @@ $api->group([
             'whitelist:merchant_api'
         ],
     ], function ($api) {
+
         $api->resource('deposits', 'DepositController', ['except' => ['destroy']]);
 
         $api->resource('withdrawals', 'WithdrawalController', ['except' => ['destroy']]);
