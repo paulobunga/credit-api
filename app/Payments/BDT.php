3cb8667b4bc5f7f2db65470758a41c551dd798a5
charlie <youmax210139@gmail.com>
2022-01-28 03:44:25 +0700

    feat: random sql multi currency support

---
+++ app/Payments/BDT.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Payments;
+
+class BDT extends Base
+{
+    public static function getDepositRandomSql($request)
+    {
+        return "WITH reseller_channels AS (
+            SELECT
+                r.id AS reseller_id,
+                rbc.id AS reseller_bank_card_id,
+                pc.NAME AS channel,
+                r.credit AS credit,
+                r.currency AS currency,
+                COUNT(md.id) AS pending,
+                COALESCE(SUM(md.amount),0) AS pending_amount,
+                SUM(
+                    CASE
+                        WHEN md.amount = {$request->amount} THEN 1
+                        ELSE 0 
+                    END 
+                ) AS same_amount,
+                r.payin->>'$.pending_limit' AS pending_limit 
+            FROM
+                reseller_bank_cards AS rbc
+                LEFT JOIN resellers AS r ON rbc.reseller_id = r.id
+                LEFT JOIN payment_channels AS pc ON rbc.payment_channel_id = pc.id
+                LEFT JOIN merchant_deposits AS md ON md.reseller_bank_card_id = rbc.id AND md.status <= :md_status 
+            WHERE
+                r.currency = '{$request->currency}'
+                AND r.credit >= {$request->amount}
+                AND r.LEVEL = :r_level
+                AND r.STATUS = :r_status
+                AND r.payin->>'$.status' = :r_payin_status
+                AND r.payin->>'$.min' <= {$request->amount}
+                AND r.payin->>'$.max' >= {$request->amount}
+                AND rbc.STATUS = :rbc_status
+                AND pc.payin->>'$.status' = :pc_status
+                AND pc.currency = '{$request->currency}'
+                GROUP BY rbc.id
+            ),
+            reseller_pending AS (
+            SELECT
+                reseller_id,
+                SUM(pending_amount) AS total_pending_amount,
+                SUM(pending) AS total_pending
+                FROM
+                    reseller_channels
+                GROUP BY
+                    reseller_id 
+            ) 
+            SELECT
+                * 
+            FROM
+                reseller_channels
+                JOIN reseller_pending USING ( reseller_id ) 
+            WHERE total_pending < pending_limit
+                AND total_pending_amount + {$request->amount} <= credit
+                AND channel = '{$request->channel}' 
+                AND same_amount = 0";
+    }
+}
