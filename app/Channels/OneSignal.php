3ee0f5659079b12083bcee77908e1717f1ace321
Amr Shakya <amrshakya@gmail.com>
2022-02-18 13:32:21 +0800

    feat: broadcast one signal message to single topic

--- app/Channels/OneSignal.php
+++ app/Channels/OneSignal.php
@@ -50,38 +50,73 @@ class OneSignal
      *
      * @param mixed $notifiable
      * @param \Illuminate\Notifications\Notification $notification
+     * @param array $meta
      *
      * @return \Psr\Http\Message\ResponseInterface
      * @throws \NotificationChannels\OneSignal\Exceptions\CouldNotSendNotification
      */
-    public function send($notifiable, Notification $notification)
+    public function send($notifiable, Notification $notification, array $meta = [])
     {
-        $devices = $notifiable->devices()->get()->groupBy('platform');
-        if ($devices->isEmpty()) {
-            return;
-        }
-        $client = $this->getOneSignalClient($notifiable->getMorphClass());
+        $client = $this->getOneSignalClient(
+            !empty($meta)
+                ? $meta["model"]
+                : $notifiable->getMorphClass()
+        );
         if (!$client) {
             return;
         }
-        foreach ($devices as $platform => $dvs) {
-            $response = $client->sendNotificationCustom(
+
+        if (!empty($meta)) {
+            $response = $this->sendNotification(
+                $client,
                 $this->payload(
                     $notifiable,
                     $notification,
                     [
-                        'platform' => $platform,
-                        'include_player_ids' => $dvs->pluck('uuid')->toArray()
+                        'platform' => $meta["platform"],
+                        'tags' => $meta["targeting"]["tags"]
                     ]
                 )
             );
-
-            if ($response->getStatusCode() !== 200) {
-                Log::error($response->getBody());
-                throw CouldNotSendNotification::serviceRespondedWithAnError($response);
-            } else {
-                Log::info($response->getBody());
+        } else {
+            $devices = $notifiable->devices()->get()->groupBy('platform');
+            if ($devices->isEmpty()) {
+                return;
             }
+
+            foreach ($devices as $platform => $dvs) {
+                $response = $this->sendNotification(
+                    $client,
+                    $this->payload(
+                        $notifiable,
+                        $notification,
+                        [
+                            "platform" => $platform,
+                            "include_player_ids" => $dvs->pluck('uuid')->toArray()
+                        ]
+                    )
+                );
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * Send Notification
+     *
+     * @param  \Berkayk\OneSignal\OneSignalClient $client
+     * @param  mixed $payload
+     * @return object
+     */
+    protected function sendNotification($client, $payload)
+    {
+        $response = $client->sendNotificationCustom($payload);
+        if ($response->getStatusCode() !== 200) {
+            Log::error($response->getBody());
+            throw CouldNotSendNotification::serviceRespondedWithAnError($response);
+        } else {
+            Log::info($response->getBody());
         }
 
         return $response;
--- app/Listeners/AdminNotificationListener.php
+++ app/Listeners/AdminNotificationListener.php
@@ -3,10 +3,14 @@
 namespace App\Listeners;
 
 use App\Events\AdminNotification;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
+use App\Channels\OneSignal;
 
 class AdminNotificationListener
 {
+
+    protected $oneSignal;
     /**
      * Create the event listener.
      *
@@ -14,7 +18,7 @@ class AdminNotificationListener
      */
     public function __construct()
     {
-        //
+        $this->oneSignal = new OneSignal();
     }
 
     /**
@@ -27,5 +31,23 @@ class AdminNotificationListener
     {
         $admins = \App\Models\Admin::all();
         Notification::send($admins, $event->model);
+
+        $this->oneSignal->send(
+            $admins->first(),
+            $event->model,
+            [
+                "model" => "admin",
+                "platform" => "web",
+                "targeting" => [
+                    "tags" => [
+                        [
+                            "key" => "subscription_topic",
+                            "relation" => "=",
+                            "value" => "admin_notifications"
+                        ]
+                    ]
+                ]
+            ]
+        );
     }
 }
