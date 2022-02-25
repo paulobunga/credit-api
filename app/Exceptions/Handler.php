43a6820349f3b2465d53cbfd602b33d882aec22f
charlie <youmax210139@gmail.com>
2022-02-25 22:54:45 +0800

    feat: log validation exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,7 +7,6 @@ use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -18,7 +17,6 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         AuthorizationException::class,
-        HttpException::class,
         ModelNotFoundException::class,
         ValidationException::class,
     ];
